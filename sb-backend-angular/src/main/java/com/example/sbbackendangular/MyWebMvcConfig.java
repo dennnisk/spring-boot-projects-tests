39ef12b064ab28875b8795f1d20939a0880f7dba
Denis <dennnis.projects@gmail.com>
2021-09-06 08:51:30 -0300

    Add angular build to publish a test of spring app

---
+++ sb-backend-angular/src/main/java/com/example/sbbackendangular/MyWebMvcConfig.java
@@ -0,0 +1,24 @@
+package com.example.sbbackendangular;
+
+import org.springframework.boot.autoconfigure.web.servlet.WebMvcAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+
+/*@Configuration
+@EnableWebMvc
+@ComponentScan*/
+public class MyWebMvcConfig /*extends WebMvcAutoConfiguration*/  {
+	
+	
+/*    
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/**").addResourceLocations("/static/");
+    }
+*/    
+	
+}
